e82e79dae646afe145ea1837698e3921a942343f
Glenn Pinto <gvpinto@gmail.com>
2021-11-02 22:13:37 -0400

    First Commit: Added Firebase and FireAuth with Providers

---
+++ android/app/src/main/kotlin/com/example/flutter_shopping_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.piercingstripes.flutter_shopping
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Firebase'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => FirebaseFirestore.instance
+              .collection('testing')
+              .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
+          child: const Icon(Icons.add),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+            stream:
+                FirebaseFirestore.instance.collection('testing').snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) return const SizedBox.shrink();
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("Loading");
+              }
+              return ListView(
+                children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                  Map<String, dynamic> data =
+                      document.data()! as Map<String, dynamic>;
+                  return ListTile(
+                    title: Text(
+                        (data['timestamp'] as Timestamp).toDate().toString()),
+                  );
+                }).toList(),
+              );
+            }),
+      ),
+    );
+  }
+}
